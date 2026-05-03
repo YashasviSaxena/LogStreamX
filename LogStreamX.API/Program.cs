03a7ecea05785ac260a78cdcd4b3751b8ce03517
Yashasvi <yashasvisaxena4@gmail.com>
2026-05-03 17:42:01 +0530

    fix stable

--- LogStreamX.API/Program.cs
+++ LogStreamX.API/Program.cs
@@ -1,21 +1,35 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using LogStreamX.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// DB
+builder.Services.AddDbContext<LogDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Controllers
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<LogDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// ⭐ STATIC UI FIX (IMPORTANT)
+app.UseDefaultFiles();   // auto loads index.html
+app.UseStaticFiles();    // enables wwwroot
+
+// Root redirect (optional but clean)
+app.MapGet("/", ctx =>
+{
+    ctx.Response.Redirect("/index.html");
+    return Task.CompletedTask;
+});
 
 app.MapControllers();
 
